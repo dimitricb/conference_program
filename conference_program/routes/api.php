090ae13c9e83a5a2463e039f93283f5736293960
Bojana Dimitric <bojanadimitric00@gmail.com>
2023-06-12 04:01:08 +0200

    Izvrsena autentifikacija

--- conference_program/routes/api.php
+++ conference_program/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HotelController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,24 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
 //public routes
 
-Route::get('/hotels', 'HotelController@index');
-Route::get('/auth0/callback', '\Auth0\Login\Auth0Controller@callback')->name('auth0-callback');
-Route::get('/login', 'Auth\Auth0IndexController@login')->name('login');
-Route::get('/logout', 'Auth\Auth0IndexController@logout')->name('logout')->middleware('auth');
-
-
-Route::group(['prefix' => 'dashboard'], function () {
-    Route::view('/', 'dashboard/dashboard');
-    Route::get('reservations/create/{id}', 'ReservationController@create');
-    Route::resource('reservations', 'ReservationController')->except('create');
-});
-
+Route::get('/hotels', [HotelController::class, 'index']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 //protected routes
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('reservations/create/{id}', [ReservationController::class, 'create']);
+    Route::resource('reservations', 'ReservationController')->except('create');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});;
